ce6ba600317fe10985718465033d4498b19e1e58
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-13 11:34:00 +0800

    extras: Add new lockscreen clock fonts
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- evolution.mk
+++ evolution.mk
@@ -109,6 +109,9 @@ PRODUCT_PACKAGES += \
     ClockFontUbuntuOverlay \
     ClockFontVtksdura3dOverlay \
     ClockFontZnikomitNo24Overlay \
+    ClockFontIOSOverlay \
+    ClockFontHerculesOverlay \
+    ClockFontSlimOverlay \
     FontAccuratistOverlay \
     FontAclonicaOverlay \
     FontAmaranteOverlay \
